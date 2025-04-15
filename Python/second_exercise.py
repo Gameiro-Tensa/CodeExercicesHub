4897bc203a4372f0ea44c7055867d499ba48cb81
Steve Djiala <inf3623>
2025-04-15 19:41:10 +0200

    add file second_exercise

---
+++ Python/second_exercise.py
@@ -0,0 +1,9 @@
+
+# Ecrire un Programme Python permettant de saisir deux nombres et  d'afficher leur produit.
+
+zahl1 = int(input("Erste Zahl eingeben: "))
+zahl2 = int(input("Zweite Zahl eingeben: "))
+
+zahl3 = zahl1 * zahl2
+print(f"Das Produkt ist: {zahl3}")
+
